758805d88ebe4f48095a15f6f26986d815f958f2
sue7ga <sue77ga@gmail.com>
2014-05-15 05:04:13 -0700

    group

--- station.rb
+++ station.rb
@@ -2,6 +2,10 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+module JSON
+ 
+end
+
 class Pref
 
   def initialize(prefid = 14)
@@ -25,22 +29,65 @@ class Pref
 
 end
 
-pref = Pref.new(13)
-station =  pref.show
+class Line
 
-line = station['line']
+  def initialize(linecode = 11302)
+    @linecd = linecode
+  end
+  
+  def linecd
+    @linecd
+  end
+
+  def show 
+   @linecd = self.linecd
+    uri = URI.parse("http://www.ekidata.jp/api/l/" + "#{@linecd}"+ ".json")
+    json = Net::HTTP.get(uri)
+    json =  json.gsub(/xml.data = /,"")
+    json =  json.gsub(/if\(typeof\(xml.onload\)=='function'\) xml.onload\(xml.data\);/,"")
+    json =  json.gsub(/if\(typeof\(xml\)=='undefined'\) xml = {};/,"")
+    result = JSON.parse(json)
+    return result
+  end
+
+end
+
+class Station
+
+ def initialize(stationcd = 1130224)
+   @stationcode = stationcd
+ end
+
+ def stationcode
+   @stationcode
+ end
+
+ def show
+  @stationcode = self.stationcode
+  uri = URI.parse("http://www.ekidata.jp/api/s/" + "#{@stationcode}" + ".json")
+  json = Net::HTTP.get(uri)   
+  json =  json.gsub(/xml.data = /,"")
+  json =  json.gsub(/if\(typeof\(xml.onload\)=='function'\) xml.onload\(xml.data\);/,"")
+  json =  json.gsub(/if\(typeof\(xml\)=='undefined'\) xml = {};/,"")
+  result = JSON.parse(json)
+  return result
+ end
+
+end
+
+class Group
+
+end
+
+station = Station.new
+station = station.show
+
+line =  station['station']
 
 line.each{
-   |line| 
-   puts line['line_cd'],line['line_name'],"\n"
+ |line|
+ puts line['line_name']
 }
-
-
-
-
-
-
-
 
 
 
